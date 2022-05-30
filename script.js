b287721739a570b451fd23a1240928eb5f640453
Adriano Misina <adriano.misina@gmail.com>
2022-05-30 08:59:51 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.item').forEach(item=> {
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragend', dragEnd);
+});
+
+//Function Item
+function dragStart(event) {
+
+}
+
+function dragEnd(event) {
+
+}
+
+//Function Area
